1f629ee22d0148d60a231a67b5c866f218f105a8
Danielle Bergbower <dkw5011@gmail.com>
2020-04-05 04:38:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Starting my server using Shotgun!"
   end
 
-get '/' do
-  "Welcome to your app!!!! I BUILT THIS!"
-end
 end
